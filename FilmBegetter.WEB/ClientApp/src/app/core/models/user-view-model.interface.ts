dd1c9badaafe8e84ad96dab7ca743521692cd00a
Yevhen Chubarov <40770350+Nexus20@users.noreply.github.com>
2022-06-21 19:19:46 +0300

    Added angular models for movie collections, ratings, roles and subscriptions

--- FilmBegetter.WEB/ClientApp/src/app/core/models/user-view-model.interface.ts
+++ FilmBegetter.WEB/ClientApp/src/app/core/models/user-view-model.interface.ts
@@ -1,2 +1,23 @@
+import { CommentViewModel } from "./commentViewModel.interface";
+import { SubscriptionViewModel } from "./subscription-view-model.interface";
+import { RoleViewModel } from "./role-view-model.interface";
+import { RatingViewModel } from "./rating-view-model.interface";
+import { MovieCollectionViewModel } from "./movie-collection-view-model.interface";
+
 export interface UserViewModel {
+
+    id: string;
+    name: string;
+    surname: string;
+    subscriptionExpirationDare?: Date;
+    username: string;
+    email: string;
+    isBanned: boolean;
+    unbanDate?: Date;
+    subscription: SubscriptionViewModel;
+    roles: RoleViewModel[];
+    comments: CommentViewModel[];
+    friends: UserViewModel[];
+    ratings: RatingViewModel[];
+    movieCollections: MovieCollectionViewModel[];
 }
