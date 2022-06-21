c3e41a1bc6fdebc1768fbc7ee06a51ea0c5def02
Yevhen Chubarov <40770350+Nexus20@users.noreply.github.com>
2022-06-21 19:04:05 +0300

    Injected movie service into admin/movies component for getting movies when component inits

--- FilmBegetter.WEB/ClientApp/src/app/core/pages/admin-page/movies/movies.component.ts
+++ FilmBegetter.WEB/ClientApp/src/app/core/pages/admin-page/movies/movies.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService } from "../../../services/movie.service";
+import { HttpErrorResponse } from "@angular/common/http";
+import { MovieViewModel } from "../../../models/movieViewModel.interface";
 
 @Component({
   selector: 'app-movies',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private movieService: MovieService) { }
+
+    public getMovies = () => {
+        this.movieService.getMovies("api/movies").subscribe({
+            next: (data: {$id: string, $values: MovieViewModel[]}) => {
+                console.log(data);
+            },
+            error: (err: HttpErrorResponse) => {
+                console.log(err);
+            }
+        });
+    }
 
   ngOnInit(): void {
+      this.getMovies();
   }
 
 }
