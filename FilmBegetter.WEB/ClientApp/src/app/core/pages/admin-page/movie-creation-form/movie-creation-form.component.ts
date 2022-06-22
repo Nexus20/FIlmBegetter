00fe66dcc92813dd7f273277119d50a2e2d47576
Yevhen Chubarov <40770350+Nexus20@users.noreply.github.com>
2022-06-23 01:49:04 +0300

    Added genre backend service Added genre frontend service Added api endpoints for getting genres

--- FilmBegetter.WEB/ClientApp/src/app/core/pages/admin-page/movie-creation-form/movie-creation-form.component.ts
+++ FilmBegetter.WEB/ClientApp/src/app/core/pages/admin-page/movie-creation-form/movie-creation-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { GenreViewModel } from "../../../models/genreViewModel.interface";
+import { GenreService } from "../../../services/genre.service";
+import { HttpErrorResponse } from "@angular/common/http";
 
 @Component({
   selector: 'app-movie-creation-form',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieCreationFormComponent implements OnInit {
 
-  constructor() { }
+    movieForm!: FormGroup;
 
-  ngOnInit(): void {
-  }
+    genres!: GenreViewModel[];
 
+    constructor(private genreService: GenreService) { }
+
+    ngOnInit(): void {
+
+        this.movieForm = new FormGroup( {
+            title: new FormControl('', [Validators.required]),
+            description: new FormControl('', [Validators.required]),
+            country: new FormControl('', [Validators.required]),
+            director:  new FormControl('', [Validators.required]),
+            publicationDate:  new FormControl('', [Validators.required]),
+            genres: new FormControl('', [Validators.required]),
+            image: new FormControl()
+        });
+
+        this.getGenres();
+    }
+
+    public getGenres = () => {
+        this.genreService.getGenres("api/genres").subscribe({
+            next: (data: GenreViewModel[]) => {
+                console.log(data);
+            },
+            error: (err: HttpErrorResponse) => {
+                console.log(err);
+            }
+        });
+    }
 }
