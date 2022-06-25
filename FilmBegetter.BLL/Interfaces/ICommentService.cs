f1f2049c8f6952fccfcb6ff43f3c574ec7ffcac3
Yevhen Chubarov <40770350+Nexus20@users.noreply.github.com>
2022-06-25 22:11:23 +0300

    ICommentService.cs

---
+++ FilmBegetter.BLL/Interfaces/ICommentService.cs
@@ -0,0 +1,9 @@
+﻿using FilmBegetter.BLL.Dto;
+using FilmBegetter.DAL.Entities;
+
+namespace FilmBegetter.BLL.Interfaces;
+
+public interface ICommentService {
+    
+    Task<CommentDto> CreateCommentAsync(CommentDto dto);
+}
