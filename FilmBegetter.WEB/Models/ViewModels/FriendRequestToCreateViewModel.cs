cfc690f68ccb9173bbeb1ef110d6fd21e5ca15cc
Yevhen Chubarov <40770350+Nexus20@users.noreply.github.com>
2022-06-28 18:55:17 +0300

    ViewModel

---
+++ FilmBegetter.WEB/Models/ViewModels/FriendRequestToCreateViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FilmBegetter.WEB.Controllers;
+
+public class FriendRequestToCreateViewModel {
+
+    public string SenderId { get; set; }
+        
+    public string RecipientId { get; set; }
+}
