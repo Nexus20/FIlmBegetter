3467a7b1d5d82eff17ab09c2f332b6996c55794e
--global <--global>
2022-06-29 23:39:53 +0300

    - implemented sidemenu for admin and user profile

--- FilmBegetter.WEB/ClientApp/src/app/core/pages/admin-page/home/home.component.ts
+++ FilmBegetter.WEB/ClientApp/src/app/core/pages/admin-page/home/home.component.ts
@@ -1,3 +1,4 @@
+import { ISideMenu } from './../../../../shared/components/side-menu/side-menu.interface';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+
+  public routes: ISideMenu[] = [
+    {
+      icon: 'video-play',
+      route: 'movies',
+      label: 'Movies'
+    },
+    {
+      icon: 'video-play',
+      route: 'movies/new',
+      label: 'New movie'
+    },
+    {
+      icon: 'person',
+      route: 'users',
+      label: 'Users'
+    },
+    {
+      icon: 'statistics',
+      route: '',
+      label: 'Statistics'
+    },
+
+  ];
 
   constructor() { }
 
