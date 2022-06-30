9c1a5c6978f6019c2674df6798387232f4148871
Сергій Гладкий <serhii.hladkyi@nure.ua>
2022-06-30 18:42:15 +0300

    Implemented endpoint for getting all roles

--- FilmBegetter.WEB/Controllers/RolesController.cs
+++ FilmBegetter.WEB/Controllers/RolesController.cs
@@ -2,6 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using FilmBegetter.BLL.Dto;
+using FilmBegetter.BLL.Interfaces;
+using FilmBegetter.Domain;
+using FilmBegetter.WEB.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,12 +16,24 @@ namespace FilmBegetter.WEB.Controllers;
 [Route("api/[controller]")]
 [ApiController]
 public class RolesController : ControllerBase {
+
+    private readonly IRoleService _roleService;
+
+    private readonly IMapper _mapper;
         
     // GET: api/Roles
+    public RolesController(IRoleService roleService, IMapper mapper) {
+        _roleService = roleService;
+        _mapper = mapper;
+    }
+
     [HttpGet]
-    public IEnumerable<string> Get()
-    {
-        return new string[] { "value1", "value2" };
+    [Authorize(Roles = UserRoles.Admin)]
+    public async Task<IActionResult> Get() {
+
+        var source = await _roleService.GetAllRolesAsync();
+
+        return Ok(_mapper.Map<List<RoleDto>, List<RoleViewModel>>(source));
     }
 
     // POST: api/Roles
